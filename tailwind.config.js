b477f5111d61800b0067431d0b379e8657b9b6ce
Olaf Willner <olafdrw@gmail.com>
2021-10-18 21:55:13 -0400

    update

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [
+    './*.html'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+      serif: ['FreightTextProBook', 'serif'],
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
